e69f6e43d39e20f7edbd6eb92ad7688343f23d98
kdani7777 <kdani7777@yahoo.com>
2021-11-03 14:05:52 -0500

    initial read functionality. unsure if it works yet.

--- src/app/pages/tabs/shareTab/tab1.page.ts
+++ src/app/pages/tabs/shareTab/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NFC, Ndef } from '@ionic-native/nfc/ngx';
+// might need to downgrade to import { NFC, NDef } from '@ionic-native/nfc'
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,37 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private nfc: NFC, private ndef: Ndef) {}
+
+  onReadClick(nfc) {
+    this.nfc.addNdefListener(() => {
+      console.log('successfully attached ndef listener');
+    }, (err) => {
+      console.log('error attaching ndef listener', err);
+    }).subscribe((event) => {
+      console.log('received ndef message. data in tag is: ', event.tag);
+      console.log('payload content is: ', this.nfc.bytesToString(event.tag.ndefMessage[0].payload));
+      console.log('decoded tag id: ', this.nfc.bytesToHexString(event.tag.id));
+      // const message = [this.ndef.textRecord('hello, world')];
+      // this.nfc.share(message);
+      // console.log(JSON.stringify(tag));
+    }, (err) => console.log(err));
+
+
+  }
+
+  onWriteClick(nfc) {
+    console.log(nfc.tag);
+
+    const message = [ this.ndef.textRecord('hello world') ];
+
+    nfc.write(
+      message,
+      success => console.log('wrote data to tag'),
+      error => console.log(error)
+    );
+  }
+
+  // this.nfc.addNdefListener(onWriteClick);
 
 }
