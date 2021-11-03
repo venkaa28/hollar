0922d048da86d4c68e09748fa7c41b25fc04d426
kdani7777 <kdani7777@yahoo.com>
2021-11-03 15:48:48 -0500

    updated read functionality. write functionality for android only.

--- src/app/pages/tabs/shareTab/tab1.page.ts
+++ src/app/pages/tabs/shareTab/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NFC, Ndef } from '@ionic-native/nfc/ngx';
+import { Platform } from '@ionic/angular';
 // might need to downgrade to import { NFC, NDef } from '@ionic-native/nfc'
 
 @Component({
@@ -9,35 +10,55 @@ import { NFC, Ndef } from '@ionic-native/nfc/ngx';
 })
 export class Tab1Page {
 
-  constructor(private nfc: NFC, private ndef: Ndef) {}
+  constructor(private nfc: NFC, private ndef: Ndef, public platform: Platform) {}
 
-  onReadClick(nfc) {
+  onReadClick() {
+    // check if device is android or ios
+    if (this.platform.is('android')) {
+      // read tag using android device
+      this.nfc.addNdefListener(() => {
+        console.log('successfully attached ndef listener');
+      }, (err) => {
+        console.log('error attaching ndef listener', err);
+      }).subscribe((event) => {
+        console.log('received ndef message. data in tag is: ', event.tag);
+        console.log('payload content is: ', this.nfc.bytesToString(event.tag.ndefMessage[0].payload));
+        console.log('decoded tag id: ', this.nfc.bytesToHexString(event.tag.id));
+        // const message = [this.ndef.textRecord('hello, world')];
+        // this.nfc.share(message);
+        // console.log(JSON.stringify(tag));
+      }, (err) => console.log(err));
+    } else if (this.platform.is('ios')) {
+      // read tag using ios device
+      this.nfc.scanNdef().then(
+        tag => console.log(JSON.stringify(tag)),
+        error => console.log(error)
+      );
+
+    }
+
+  }
+
+  onWriteClick(nfc) {
     this.nfc.addNdefListener(() => {
       console.log('successfully attached ndef listener');
     }, (err) => {
       console.log('error attaching ndef listener', err);
     }).subscribe((event) => {
-      console.log('received ndef message. data in tag is: ', event.tag);
-      console.log('payload content is: ', this.nfc.bytesToString(event.tag.ndefMessage[0].payload));
-      console.log('decoded tag id: ', this.nfc.bytesToHexString(event.tag.id));
-      // const message = [this.ndef.textRecord('hello, world')];
-      // this.nfc.share(message);
-      // console.log(JSON.stringify(tag));
+      // writing to android
+      if (this.platform.is('android')) {
+        const message = [];
+        const tnf = this.ndef.TNF_EXTERNAL_TYPE;
+        const recordType = 'android.com:pkg';
+        const payload = 'com.jwsoft.nfcactionlauncher'; // change to our Hollar AAR (android application record)
+        const record = this.ndef.record(tnf, recordType, [], payload);
+        message.push(record);
+        this.nfc.write(message).then(r => console.log(r));
+      }
+
+
+      // this.nfc.write(message);
     }, (err) => console.log(err));
-
-
-  }
-
-  onWriteClick(nfc) {
-    console.log(nfc.tag);
-
-    const message = [ this.ndef.textRecord('hello world') ];
-
-    nfc.write(
-      message,
-      success => console.log('wrote data to tag'),
-      error => console.log(error)
-    );
   }
 
   // this.nfc.addNdefListener(onWriteClick);
