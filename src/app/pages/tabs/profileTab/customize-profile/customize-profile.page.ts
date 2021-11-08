ec093f8aea2a71a6a4c99f4e92f3e184eec9adbb
brumm-ryan <71672558+brumm-ryan@users.noreply.github.com>
2021-11-08 15:16:02 -0600

    Added routing from tab3 to customize profile

--- src/app/pages/tabs/profileTab/customize-profile/customize-profile.page.ts
+++ src/app/pages/tabs/profileTab/customize-profile/customize-profile.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {UserProfile} from "../../../../../models/userProfile.model";
+import {Observable} from "rxjs";
+import * as UserActions from "../../../../stores/userStore/userActions";
+import {selectUserObs} from "../../../../stores/userStore/userReducer";
+import {AuthService} from "../../../../services/auth.service";
+import {Store} from "@ngrx/store";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-customize-profile',
@@ -7,9 +14,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomizeProfilePage implements OnInit {
 
-  constructor() { }
+  currentUser: UserProfile;
+  currentUserObs: Observable<any>;
+
+  constructor(
+    private authService: AuthService,
+    private store: Store<any>,
+    private route: Router
+  ) { }
 
   ngOnInit() {
+    this.store.dispatch(new UserActions.FetchUser());
+    this.currentUserObs = this.store.select(selectUserObs);
+    this.currentUserObs.subscribe( (value: UserProfile) => {
+      this.currentUser = value;
+    });
   }
 
 }
