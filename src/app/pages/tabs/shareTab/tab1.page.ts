8c054984523dfa212ffa89d93e4f9f972c11f24e
Ani <anirudh2000@gmail.com>
2021-11-10 13:15:55 -0600

    added scaffolding for handleRead function

--- src/app/pages/tabs/shareTab/tab1.page.ts
+++ src/app/pages/tabs/shareTab/tab1.page.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { NFC, Ndef } from '@ionic-native/nfc/ngx';
 import { Platform } from '@ionic/angular';
+import {UserProfile} from '../../../../models/userProfile.model';
+import {Observable} from 'rxjs';
+import * as UserActions from '../../../stores/userStore/userActions';
+import {selectUserObs} from '../../../stores/userStore/userReducer';
+import {Store} from '@ngrx/store';
 // might need to downgrade to import { NFC, NDef } from '@ionic-native/nfc/ngx'
 
 @Component({
@@ -8,10 +13,22 @@ import { Platform } from '@ionic/angular';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor(private nfc: NFC, private ndef: Ndef, public platform: Platform) {
-    nfc.showSettings().then(r => console.log(r));
+  currentUser: UserProfile;
+  currentUserObs: Observable<any>;
+
+  constructor(private nfc: NFC, private ndef: Ndef, public platform: Platform, private store: Store<any>) {
+    //nfc.showSettings().then(r => console.log(r));
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(new UserActions.FetchUser());
+    this.currentUserObs = this.store.select(selectUserObs);
+    this.currentUserObs.subscribe( (value: UserProfile) => {
+      this.currentUser = value;
+    });
+
   }
 
   onReadClick() {
@@ -32,16 +49,18 @@ export class Tab1Page {
       // }, (err) => console.log(err));
     } else if (this.platform.is('ios')) {
       // read tag using ios device
-      console.log("this is an ios device");
+      console.log('this is an ios device');
       this.nfc.scanTag().then(
         tag => {
-          console.log("reader session started");
+          console.log('reader session started');
           console.log(JSON.stringify(tag));
           if (tag.id) {
             const message = tag.ndefMessage;
-            console.log(message);
-            alert(this.nfc.bytesToString(message[0].payload).substring(3));
+            //console.log(message);
+            const uid = this.nfc.bytesToString(message[0].payload).substring(3);
+            alert(uid);
             console.log(this.nfc.bytesToString(message[0].payload).substring(3));
+            this.handleRead(uid);
           }
         },
         error => console.log(error)
@@ -53,11 +72,22 @@ export class Tab1Page {
     this.nfc.addNdefListener(() => {
       console.log('successfully attached ndef listener');
       //only this works
-      const message = [this.ndef.textRecord('insert UID here')];
+      const message = [this.ndef.textRecord(this.currentUser.uid)];
       console.log(message);
       this.nfc.write(message).then(r => console.log(r));
     }, (err) => {
       console.log('error attaching ndef listener', err);
     });
+  }
+
+  handleRead(uid){
+    if(uid === this.currentUser.uid){
+      alert('You cannot connect with yourself!');
+    } else if (uid in this.currentUser.connections){
+      alert('You are already connected with this user');
+    }else {
+      alert('going to connect you with new user if this is a valid uid ');
+      //call create connection here
+    }
   }
 }
